b2630492dffa6570ed385ec7bf2f6cabdc3be950
barabadi <101886037+barabadi@users.noreply.github.com>
2023-02-06 18:35:22 +0200

    Add files via upload

---
+++ src/Counter.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(1);
+
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  }, [count]);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
